b72b3d55495ade68314cb6d7bbccb6928b99f436
Pedro Peçanha <60028123+pedromp-02@users.noreply.github.com>
2022-04-25 17:12:51 -0300

    teste dois asserts com funcaoWlad

--- testes.py
+++ testes.py
@@ -1,4 +1,5 @@
 from funcaoTeste import *
+from funcaoWlad import *
 
 def test_add():
     assert add(2, 3) == 5
@@ -8,4 +9,7 @@ def test_subtract():
     assert subtract(2, 3) == -1
     
 def test_subtract_2():
-    assert subtract(5, 3) == 2
+    numero = subtract (4,2)
+    assert numero == 2
+    funcaoWlad(numero)
+    assert numero == 4
